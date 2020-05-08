6ece4514821771de572d3f872b327a2de50f38ee
Kevin Barrios <48795560+KvnBarrios@users.noreply.github.com>
2020-05-08 01:46:29 -0300

    Nova tela login
    Nova tela do login com novo logo tipo

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,42 +10,89 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[850],
+      backgroundColor: Colors.deepPurple,
       body: Stack(
         alignment: Alignment.center,
         children: <Widget>[
           Container(),
           SingleChildScrollView(
             child: Container(
-              margin: EdgeInsets.all(16),
+              margin: EdgeInsets.only(
+                top: 10,
+                left: 26,
+                right: 26,
+                bottom: 120
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Icon(
-                    Icons.store_mall_directory,
-                    color: Colors.deepPurple,
-                    size: 160,
-                  ),
-                  InputField(
-                    icon: Icons.person_outline,
-                    hint:"Usuário",
-                    obscure: false,
-                  ),
-                  InputField(
-                    icon: Icons.lock_outline,
-                    hint:"Senha",
-                    obscure: true,
+                  Image(
+                    image: AssetImage("images/logo.png"),
+                    color: Colors.black,
+                    height: 200,
                   ),
                   SizedBox(
-                    height: 32,
+                    height: 70,
                   ),
-                  SizedBox(
-                    height: 50,
-                    child: RaisedButton(
-                      color: Colors.deepPurple,
-                      child: Text("Entrar"),
-                      onPressed: (){},
-                      textColor: Colors.white,
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                        bottomLeft: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0)
+                      )
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 50,
+                        ),
+                        InputField(
+                          icon: Icons.person_outline,
+                          hint:"Usuário",
+                          obscure: false,
+                        ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                        InputField(
+                          icon: Icons.lock_outline,
+                          hint:"Senha",
+                          obscure: true,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          height: 60,
+                          width: 200,
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: Colors.deepPurple)
+                            ),
+                            color: Colors.deepPurple,
+                            child: Text("Entrar"),
+                            onPressed: (){},
+                            textColor: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("Esqueceu a sua senha ?",
+                          style: TextStyle(color: Colors.deepPurple, fontSize: 15),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
                     ),
                   ),
                 ],
--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -10,23 +10,31 @@ class InputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        icon: Icon(icon, color: Colors.white,),
-        hintText: hint,
-        hintStyle: TextStyle(color: Colors.white),
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: Colors.deepPurple)
-        ),
-        contentPadding: EdgeInsets.only(
-          left: 5,
+    return Container(
+        margin: EdgeInsets.only(
+          top: 10,
+          left: 30,
           right: 30,
-          bottom: 30,
-          top: 30
-        )
+          bottom: 0
+        ),
+      child: TextField(
+        decoration: InputDecoration(
+          icon: Icon(icon, color: Colors.deepPurple,),
+          hintText: hint,
+          hintStyle: TextStyle(color: Colors.deepPurple),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple)
+          ),
+          contentPadding: EdgeInsets.only(
+            left: 5,
+            right: 10,
+            bottom: 10,
+            top: 5
+          )
+        ),
+        style: TextStyle(color: Colors.deepPurple),
+        obscureText: obscure,
       ),
-      style: TextStyle(color: Colors.white),
-      obscureText: obscure,
     );
   }
 }
