b5a0d977936a50fbe2a353c849762cf3067663d8
Kevin Barrios <48795560+KvnBarrios@users.noreply.github.com>
2020-05-09 00:18:29 -0300

    Layout feio

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,38 +3,128 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+    title: const Text('Resumo diario'),
+        backgroundColor: Colors.grey,
+        centerTitle: true,
+    ),
+    body: GridView.count(
+    // Create a grid with 2 columns. If you change the scrollDirection to
+    // horizontal, this produces 2 rows.
+    crossAxisCount: 1,
+    crossAxisSpacing: 0,
+    padding: EdgeInsets.only(bottom: 90),
+    childAspectRatio: 2.5,
 
-    Widget _buildBodyBack() => Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors:[
-            Colors.deepPurple,
-            Colors.purple
+    // Generate 100 widgets that display their index in the List.
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.indigo, Colors.lightBlueAccent]),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+                bottomLeft: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0)
+            ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-        )
+        ),
+        child: SizedBox(child: FlatButton(child: Text("PEDIDOS: 9", style: TextStyle(fontSize: 36,color: Colors.white),), onPressed: () {
+        },)),
       ),
-    );
-    return Stack(
-      children: <Widget>[
-        _buildBodyBack(),
-        CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              floating: true,
-              snap: true,
-              backgroundColor: Colors.transparent,
-              elevation: 0.0,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text("Resumo do dia"),
-                centerTitle: true,
-              ),
-            )
+      Container(
+        margin: EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.deepPurple, Colors.purple]),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+                bottomLeft: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0)
+            ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
           ],
-        )
-      ],
+        ),
+        child: SizedBox(child: FlatButton(child: Text("CAIXA RS 420.00", style: TextStyle(fontSize: 36,color: Colors.white),), onPressed: () {
+        },)),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.red, Colors.redAccent]),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+                bottomLeft: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0)
+            ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: SizedBox(child: FlatButton(child: Text("PRODUÇÃO", style: TextStyle(fontSize: 36,color: Colors.white),), onPressed: () {
+        },)),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.green, Colors.green[400]]),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+                bottomLeft: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0)
+            ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: SizedBox(child: FlatButton(child: Text("ESTOQUE", style: TextStyle(fontSize: 36,color: Colors.white),), onPressed: () {
+        },)),
+      )
+    ],
+    )
     );
-
  }
 }
